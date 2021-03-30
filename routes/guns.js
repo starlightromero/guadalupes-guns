ba8326086c06f3b5e7bf134691f03374240527fa
Starlight Romero <starlightromero@protonmail.com>
2021-03-29 20:13:24 -0700

    [create] guns routes

---
+++ routes/guns.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const router = express.Router()
+
+const gunsController = require('./controllers/guns')
+
+router.get('/guns/:id', gunsController.getGunById)
+
+router.get('/guns/new', gunsController.getNewGunForm)
+
+router.post('/guns', gunsController.createGun)
+
+router.get('/guns/:id/edit', gunsController.getUpdateGunForm)
+
+router.patch('/guns/:id', gunsController.updateGun)
+
+router.delete('/guns/:id', gunsController.deleteGun)
+
+module.exports = router
