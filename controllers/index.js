5d23934a29c10b686b4462438d174f63cf172999
Starlight Romero <starlightromero@protonmail.com>
2021-03-30 08:51:23 -0700

    [update] added pagination for search

--- controllers/index.js
+++ controllers/index.js
@@ -13,9 +13,16 @@ exports.getAllGuns = async (req, res) => {
 }
 
 exports.searchGuns = async (req, res) => {
+  const currentPage = req.query.page || 1
+  const perPage = 2
   const term = new RegExp(req.query.term, 'i')
+  const count = await Gun.find().countDocuments()
   const guns = await Gun.find({
     $or: [{ model: term }, { firingMode: term }, { caliber: term }]
+  }).skip((currentPage - 1) * perPage)
+  res.render('guns-index', {
+    guns: guns,
+    pagesCount: count / perPage,
+    currentPage: currentPage
   })
-  res.render('guns-index', { guns: guns })
 }
