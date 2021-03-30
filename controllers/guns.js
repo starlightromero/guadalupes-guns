1cca6178e522aa3a0951f6dc8f4f406b51145d2c
Starlight Romero <starlightromero@protonmail.com>
2021-03-29 20:10:48 -0700

    [create] guns controller

---
+++ controllers/guns.js
@@ -0,0 +1,31 @@
+const Gun = require('./models/gun')
+
+exports.getGunById = async (req, res) => {
+  const gun = await Gun.findById(req.params.id)
+  res.render('guns-show', { gun: gun })
+}
+
+exports.getNewGunForm = (req, res) => {
+  res.render('guns-new')
+}
+
+exports.createGun = async (req, res) => {
+  const gun = new Gun(req.body)
+  await gun.save()
+  res.redirect(`/guns/${gun._id}`)
+}
+
+exports.getUpdateGunForm = async (req, res) => {
+  const gun = await Gun.findById(req.params.id)
+  res.render('guns-edit', { gun: gun })
+}
+
+exports.updateGun = async (req, res) => {
+  const gun = await Gun.findByIdAndUpdate(req.params.id, req.body)
+  res.redirect(`/guns/${gun._id}`)
+}
+
+exports.deleteGun = async (req, res) => {
+  await Gun.findByIdAndRemove(req.params.id)
+  return res.redirect('/')
+}
