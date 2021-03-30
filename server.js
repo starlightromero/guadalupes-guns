dd0acbc7c2c92e669aa258f9762a390781009c76
Starlight Romero <starlightromero@protonmail.com>
2021-03-29 23:33:00 -0700

    [update] server added route prefix

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', gunsRoutes)
+app.use('/guns', gunsRoutes)
 app.use('/', indexRoutes)
 
 // catch 404 and forward to error handler
