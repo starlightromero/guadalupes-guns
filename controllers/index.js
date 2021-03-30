f0d95727b1dcab3df41664e12a20e6ca3789133c
Starlight Romero <starlightromero@protonmail.com>
2021-03-29 23:34:01 -0700

    [update] added searchGuns controller

--- controllers/index.js
+++ controllers/index.js
@@ -4,3 +4,11 @@ exports.getAllGuns = async (req, res) => {
   const guns = await Gun.find()
   res.render('guns-index', { guns: guns })
 }
+
+exports.searchGuns = async (req, res) => {
+  const term = new RegExp(req.query.term, 'i')
+  const guns = await Gun.find({
+    $or: [{ model: term }, { firingMode: term }, { caliber: term }]
+  })
+  res.render('guns-index', { guns: guns })
+}
