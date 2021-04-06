f5b727023c3bd8ce7c01dc6919dba390ee4904db
Starlight Romero <starlightromero@protonmail.com>
2021-04-05 21:16:19 -0700

    [create] purchaseGun controller

--- controllers/guns.js
+++ controllers/guns.js
@@ -2,6 +2,8 @@ const { validationResult } = require('express-validator')
 
 const Gun = require('../models/gun')
 
+const mailer = require('../utils/mailer')
+
 exports.getGunById = async (req, res) => {
   try {
     const gun = await Gun.findById(req.params.id)
@@ -69,5 +71,41 @@ exports.deleteGun = async (req, res) => {
     return res.redirect('/')
   } catch (err) {
     throw new Error(err)
+  }
+}
+
+exports.purchaseGun = async (req, res) => {
+  console.log(req.body)
+  // Set your secret key: remember to change this to your live secret key in production
+  // See your keys here: https://dashboard.stripe.com/account/apikeys
+  const stripe = require('stripe')(process.env.PRIVATE_STRIPE_API_KEY)
+
+  // Token is created using Checkout or Elements!
+  // Get the payment token ID submitted by the form:
+  const token = req.body.stripeToken // Using Express
+
+  // req.body.petId can become null through seeding,
+  // this way we'll insure we use a non-null value
+  const gunId = req.body.gunId || req.params.id
+
+  try {
+    const gun = await Gun.findById(gunId)
+    const charge = stripe.charges.create({
+      amount: gun.price * 100,
+      currency: 'usd',
+      description: `Purchased ${gun.model}, ${gun.caliber}, ${gun.firingMode}`,
+      source: token,
+    })
+    // Convert the amount back to dollars for ease in displaying in the template
+    const user = {
+      email: req.body.stripeEmail,
+      amount: charge.amount / 100,
+      gunModel: gun.model,
+    }
+    // Call our mail handler to manage sending emails
+    mailer.sendMail(user, req, res)
+  } catch (err) {
+    console.log('Error: ' + err)
+    res.redirect(`/guns/${req.params.id}`)
   }
 }
