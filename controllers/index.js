2e11891102daf67111d5fbc2c4537b7f22a8cb93
Starlight Romero <starlightromero@protonmail.com>
2021-04-14 20:55:52 -0700

    [update] index contoller with full search

--- controllers/index.js
+++ controllers/index.js
@@ -18,15 +18,21 @@ exports.searchGuns = async (req, res) => {
   const currentPage = req.query.page || 1
   const perPage = 2
   const term = new RegExp(req.query.term, 'i')
-  const count = await Gun.find().countDocuments()
-  const guns = await Gun.find({
-    $or: [{ model: term }, { firingMode: term }, { caliber: term }],
-  })
-    .skip((currentPage - 1) * perPage)
-    .limit(perPage)
-  res.status(200).render('guns-index', {
-    guns: guns,
-    pagesCount: count / perPage,
-    currentPage: currentPage,
-  })
+  try {
+    const count = await Gun.find().countDocuments()
+    const guns = await Gun.find(
+      { $text: { $search: term } },
+      { score: { $meta: 'textScore' } }
+    )
+      .sort({ score: { $meta: 'textScore' } })
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage)
+    res.status(200).render('guns-index', {
+      guns: guns,
+      pagesCount: count / perPage,
+      currentPage: currentPage,
+    })
+  } catch (err) {
+    return res.status(400).send(err)
+  }
 }
